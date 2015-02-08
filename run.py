495a0dae7924f1a315ec86e2d56a5d28276f8e19
sargentfrancesca <f.sargent@exeter.ac.uk>
2015-02-08 18:17:41 +0000

    initial commit w/examples

---
+++ run.py
@@ -0,0 +1,80 @@
+import os, sys
+from urllib2 import urlopen
+import hashlib, re, urllib
+from json import load 
+
+class Book():
+	def __init__(self, title, summary, book_id, isbn10, avatar_hash, avatar_url):
+		self.title = title
+		self.summary = summary
+		self.book_id = book_id
+		self.isbn10 = isbn10
+		self.avatar_hash = avatar_hash
+		self.avatar_url = avatar_url
+
+
+def generateGravs(avatar_hash, typeof):
+	print "Generating " + typeof + "Url..."
+	url = "http://www.gravatar.com/avatar/"+avatar_hash+"?s=300&d="+typeof+"&r=g"
+	print url
+	return url 
+
+def getHashUrl(aobject):
+	b = aobject
+	avatar_hash = b.avatar_hash
+
+	identicon = generateGravs(avatar_hash, "identicon")
+	retro = generateGravs(avatar_hash, "retro")
+	monster = generateGravs(avatar_hash, "monsterid")
+
+	urllib.urlretrieve(identicon, 'img/'+b.book_id+'_identicon.png')
+	urllib.urlretrieve(retro, 'img/'+b.book_id+'_retro.png')
+	urllib.urlretrieve(monster, 'img/'+b.book_id+'_monster.png')
+
+	print "Saved images to folder"
+	return identicon
+
+def downloadBook(barcode):
+	apikey = '2QUS7QQR'
+	apiurl = 'http://isbndb.com/api/v2/json/'+apikey+'/book/'+ barcode
+	
+	print apiurl
+
+	response = urlopen(apiurl)
+	json = load(response)
+
+	try:
+		print json['data']
+	except KeyError:
+		print "Book does not exist in database. Please try another."
+		return
+	else:		
+		for book in json['data']:
+			avatar_hash = hashlib.md5(book['isbn10'].encode('utf-8')).hexdigest()
+			
+			b = Book(book['title'], book['summary'], book['book_id'], book['isbn10'], avatar_hash, '')
+			
+			b.avatar_url = getHashUrl(b)
+			
+			print b.title
+			print b.summary
+			print "Avatar hash: ", b.avatar_hash
+
+			return b
+
+a = True
+
+while a is True:
+	book = raw_input('Book Number: ')
+
+	if book == 'end':
+		sys.exit()
+	
+	else:
+		downloadBook(book)
+
+
+
+
+
+
